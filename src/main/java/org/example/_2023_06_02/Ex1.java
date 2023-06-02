5184f446e59b5d96c707b3b4b7dccc5f7e5bc0f7
shonmikl <mikhailegorov676@gmail.com>
2023-06-02 21:08:26 +0200

    Initial commit

---
+++ src/main/java/org/example/_2023_06_02/Ex1.java
@@ -0,0 +1,13 @@
+package org.example._2023_06_02;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex1 {
+    /**
+     * 1 2 5 74 8 5 4 688 458 41
+     * 1 2 3 4 5 6 7 8 9 10 11 12 ***4
+     * 0-100 50-75-87-93-96-98-99-100 ***8
+     */
+    List<Integer> list = new LinkedList<Integer>();
+}
---
+++ src/main/java/org/example/_2023_06_02/Famous.java
@@ -0,0 +1,32 @@
+package org.example._2023_06_02;
+
+public class Famous {
+    Famous getFamous(Famous[] person) {
+        Famous f = null;
+        int firstIdx = 0;
+        int lastIdx = person.length - 1;
+
+        while (firstIdx != lastIdx) {
+            if (person[firstIdx].knows(person[lastIdx])) {
+                firstIdx++;
+            } else {
+                lastIdx--;
+            }
+        }
+
+        for (int i = 0; i < person.length; i++) {
+            if (i != firstIdx
+                    && (person[i].knows(person[firstIdx])
+                    || person[firstIdx].knows(person[i]))) {
+                f = null;
+            }
+            f = person[firstIdx];
+        }
+
+        return f;
+    }
+
+    private boolean knows(Famous person) {
+        return false;
+    }
+}
